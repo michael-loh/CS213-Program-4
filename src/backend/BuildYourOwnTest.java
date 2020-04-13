58a7879d8185046df42e1b107db98eed36f7ec8e
michael-loh <54383090+michael-loh@users.noreply.github.com>
2020-04-13 13:45:04 -0400

    Add files via upload

--- src/backend/BuildYourOwnTest.java
+++ src/backend/BuildYourOwnTest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package backend;
 
 import static org.junit.Assert.*;
@@ -10,7 +7,7 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 /**
- * @author Varun Vasudevan
+ * @author Varun Vasudevan and Michael Loh
  *
  */
 public class BuildYourOwnTest {
@@ -20,25 +17,28 @@ public class BuildYourOwnTest {
 	 */
 	@Test
 	public void testPizzaPrice() {
+		//test price for pizza with different sizes
 		ArrayList<String> a = new ArrayList<String>();
 		a.add("Chicken");
 		a.add("Ham");
 		a.add("Mushroom");
-		Pizza s = new BuildYourOwn("BuildYourOwn","Small",a);
-		assertEquals(11,s.pizzaPrice());
-		Pizza m = new BuildYourOwn("BuildYourOwn","Medium",a);
-		assertEquals(13,m.pizzaPrice());
-		Pizza l = new BuildYourOwn("BuildYourOwn","Large",a);
-		assertEquals(15,l.pizzaPrice());
-		a.add("Green Pepper");
-		a.add("Pepperoni");
-		Pizza s2 = new BuildYourOwn("BuildYourOwn","Small",a);
-		assertEquals(15,s2.pizzaPrice());
+		Pizza s1 = new BuildYourOwn("BuildYourOwn","Small",a);
+		assertEquals(11,s1.pizzaPrice());
+		Pizza s2 = new BuildYourOwn("BuildYourOwn","Medium",a);
+		assertEquals(13,s2.pizzaPrice());
+		Pizza s3 = new BuildYourOwn("BuildYourOwn","Large",a);
+		assertEquals(15,s3.pizzaPrice());
+		
+		//test price for pizza with different number of toppings 
 		ArrayList<String> b = new ArrayList<String>();
 		b.add("Chicken");
-		Pizza s3 = new BuildYourOwn("BuildYourOwn","Small",b);
-		assertEquals(7,s3.pizzaPrice());
-		//fail("Not yet implemented");
+		Pizza t1 = new BuildYourOwn("BuildYourOwn","Small",b);
+		assertEquals(7, t1.pizzaPrice());
+		b.add("Cheese");
+		Pizza t2 = new BuildYourOwn("BuildYourOwn", "Small",b);
+		assertEquals(9, t2.pizzaPrice());
+		
+		
 	}
 
 }
